249450c4207d17720a7a571f2e3963f0d511aa8c
Jonathan Claudius <jclaudius@trustwave.com>
2014-07-13 14:52:01 -0400

    Fix indentation of Downloadr::HTTP

--- lib/downloadr/http.rb
+++ lib/downloadr/http.rb
@@ -4,22 +4,22 @@ require "uri"
 module Downloadr
   # HTTP/HTTPS Transport handler for downloading a file
   class HTTP
-  	attr_reader :uri, :path
+    attr_reader :uri, :path
 
-  	# @param [String] uri
-  	# @param [String] path
-  	def initialize(uri, path)
-  	  @uri = uri
-  	  @path = path
-  	end
+    # @param [String] uri
+    # @param [String] path
+    def initialize(uri, path)
+      @uri = uri
+      @path = path
+    end
 
-  	def download
-			File.write(@path, Net::HTTP.get(URI.parse(@uri)))
-  	end
+    def download
+      File.write(@path, Net::HTTP.get(URI.parse(@uri)))
+    end
 
-  	def self.download(uri, path)
-  	  downloader = Downloadr::HTTP.new(uri, path)
-  	  downloader.download
-  	end
+    def self.download(uri, path)
+      downloader = Downloadr::HTTP.new(uri, path)
+      downloader.download
+    end
   end
 end
